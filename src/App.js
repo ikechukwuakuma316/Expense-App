c140691694447aac0397bfe88bf75158a6abfd72
savvy316 <ikechukwuakuma316@gmail.com>
2022-02-24 12:53:46 +0100

    updated expenseitem

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello world!</h1>
+      <h2>Hello world!</h2>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
 import "./index.css";
 import App from "./App";
 
@@ -9,3 +10,5 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
+
+// https://meet.google.com/fbo-njha-eon

1b6179ed00a2917605c328b925c30c2f02e95c76
savvy316 <ikechukwuakuma316@gmail.com>
2022-02-24 13:02:19 +0100

    Added my project

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -13,3 +13,15 @@ function ExpenseItem() {
 export default ExpenseItem;
 
 // Adding Basic CSS Styling
+// …or create a new repository on the command line
+// echo "# FlutterClass" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/ikechukwuakuma316/FlutterClass.git
+// git push -u origin main
+// …or push an existing repository from the command line
+// git remote add origin https://github.com/ikechukwuakuma316/FlutterClass.git
+// git branch -M main
+// git push -u origin main
